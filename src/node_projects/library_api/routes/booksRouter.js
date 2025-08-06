705ddf47e7e5ec0496f2e1aea375731cc7e02515
Maksym Somushkin <syomushkin.maxim@gmail.com>
2025-08-06 11:43:56 +0200

    feat(library): add books route, update book if post the same book

--- src/node_projects/library_api/routes/booksRouter.js
+++ src/node_projects/library_api/routes/booksRouter.js
@@ -1,6 +1,16 @@
 import Book from "../models/book.js";
 
 async function booksRouter(fastify, _opts) {
+  fastify.get("/", async (req, res) => {
+    try {
+      const books = await Book.findAll();
+      res.send(books);
+    } catch (e) {
+      console.error("Error occurred:", e.message);
+      res.send(e);
+    }
+  });
+
   fastify.get("/:id", async (req, res) => {
     const { id } = req.params;
 
@@ -47,8 +57,17 @@ async function booksRouter(fastify, _opts) {
     const { title, author } = req.body;
 
     try {
-      const book = await Book.create({ title, author });
-      res.send(book);
+      const existingBook = await Book.findOne({
+        where: { title },
+      });
+      if (!existingBook) {
+        const book = await Book.create({ title, author, count: 1 });
+        res.send(book);
+      } else {
+        existingBook.count += 1;
+        await existingBook.save();
+        res.send(existingBook);
+      }
     } catch (e) {
       console.error("Error occurred:", e.message);
       res.send(e);
